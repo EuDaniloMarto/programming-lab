eb3fce74c3186af739980fc99700356bdd13be0a
DMCarvalho <dmcarvalho@tutamail.com>
2025-03-25 14:39:45 -0300

    :bulb: Foi adicionado comentários descrevendo o projeto.

---
+++ basico/d001l_exp0101.c
@@ -0,0 +1,13 @@
+// Referência:
+// [D001L] Damas, Luís, Linguagem de Programação C. 10° ed. Rio de Janeiro. LTC.
+// 2007. Exemplo 0101
+
+// Descrição: Este programa exibe a mensagem "Olá, Mundo!!!" na tela.
+
+#include <stdio.h>
+
+int
+main(void) {
+    printf("Olá, Mundo!!!\n");
+    return 0;
+}
